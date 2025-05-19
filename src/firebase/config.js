970bdcaeb181eb2412e6ddf814909bcfdb30b327
techtalentscity <techtalentscity@gmail.com>
2025-05-19 19:07:02 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export { auth, analytics };
+export { auth, analytics, db };
